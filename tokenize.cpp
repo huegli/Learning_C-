bcdeeb4e7633b16aa0af7951d776eabd481eca16
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2017-04-27 21:53:37 -0700

    More of chapter 8

---
+++ tokenize.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char the_string[81], *p;
+
+    cout << "Input a string to parse: ";
+    cin.getline(the_string, 81);
+    p = strtok(the_string, ",");
+    while(p!=NULL) {
+        cout << p << endl;
+        p = strtok(NULL, ",");
+    }
+    return 0;
+}
